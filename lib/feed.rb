4caf529931b9e9066dcd9f8f983c34a4049ae582
Justin Henry <justin@greengaloshes.cc>
2009-04-18 18:38:32 -0400

    implemented layout; added multiple feeds.  Closes gh-1

--- lib/feed.rb
+++ lib/feed.rb
@@ -1,7 +1,15 @@
 require 'feedzirra'
 class Feed
 
-  def self.delicious
-    Feedzirra::Feed.fetch_and_parse( 'http://feeds.delicious.com/v2/rss/tag/progmatica?count=15' )
+  def self.linked
+    linked_urls = ["http://feeds.delicious.com/v2/rss/kapowee/progmatica?count=8", "http://feeds.delicious.com/v2/rss/clr.portland.me/progmatica?count=8", "http://feeds.delicious.com/v2/rss/codeofficer/progmatica?count=8"]
+    Feedzirra::Feed.fetch_and_parse( linked_urls )
   end
+
+  def self.blogged
+    blogged_urls = ["http://dialogues.port49.com/feed/atom.xml", "http://www.codeofficer.com/blog/rss_2.0/" ]
+    
+    Feedzirra::Feed.fetch_and_parse( blogged_urls )
+  end
+
 end
