724abfcb2a47b3f429700366697af332ee4c8c01
Justin Henry <justin@greengaloshes.cc>
2009-05-03 17:07:46 -0400

    Hey look - real content.... beep

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,5 +2,5 @@ set :application, "progmatica"
 set :repository,  "git@github.com:progmatica/progmatica.com.git"
 set :domain, "progmatica.com"
 set :deploy_to, "/home/progmatica/public_html"
-set :user, "clr"
+set :user, "progmatica"
 
